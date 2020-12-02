6cd4e2112315456182b6cf31260e16cd9d8c7150
Thomas Eberhart <thomas.eberhart@oit.edu>
2020-12-02 10:12:47 -0800

    Added some folders and other groundwork

---
+++ NewRecord!/NewRecord_Backend/Models/Record.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewRecord_Backend.Models
+{
+    public class Record
+    {
+    }
+}
