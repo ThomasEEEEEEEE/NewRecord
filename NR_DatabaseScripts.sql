bdadcce649910dbab56ee421870e7632763e8798
Thomas <56699831+ThomasEEEEEEEE@users.noreply.github.com>
2020-12-14 09:59:26 -0800

    Update NR_DatabaseScripts.sql

--- NR_DatabaseScripts.sql
+++ NR_DatabaseScripts.sql
@@ -16,30 +16,57 @@ GO
 DROP TABLE IF EXISTS NOTIFICATIONS
 GO
 
+--Unfinished
 CREATE TABLE FRIENDS(
-
+        UserID INT NOT NULL,
+        FriendID INT NOT NULL,
+        UserName nvarchar(500) NOT NULL,
+        FriendName nvarchar(500) NOT NULL
 );
 
 CREATE TABLE USERS(
-
+        ID INT IDENTITY(1,1) NOT NULL,
+        Username nvarchar(500) UNIQUE NOT NULL,
+        PasswordHash varchar(500) NOT NULL
 );
 
 CREATE TABLE RECORDS(
-
+        UserID INT NOT NULL,
+        RecordName varchar(500) NOT NULL,
+        Image INT NOT NULL,
+        SuccessInfo varchar(50) NOT NULL,
+        Privacy varchar(50) NOT NULL
 );
 
 CREATE TABLE RECORD_HISTORY(
-
+        UserID INT NOT NULL,
+        RecordName varchar(500) NOT NULL,
+        Score FLOAT NOT NULL,
+        DateAchieved DATE NOT NULL
 );
 
 CREATE TABLE GOALS(
-
+        UserID INT NOT NULL,
+        RecordName varchar(500) NOT NULL,
+        GoalScore FLOAT NOT NULL,
+        StartDate DATE, --Nullable
+        EndDate DATE --Nullable
 );
 
+--Unfinished
 CREATE TABLE CHALLENGES(
-
+        Participants varchar(500) NOT NULL, --In the form id,id,id
+        RecordName varchar(500) NOT NULL,
+        GoalScore FLOAT NOT NULL,
+        StartDate DATE, --Nullable
+        EndDate DATE, --Nullable
+        WinnerID INT --Nullable
 );
 
+--Very Unfinished
 CREATE TABLE NOTIFICATIONS(
-
+        SenderUserID INT NOT NULL,
+        ReceiverUserID INT NOT NULL,
+        MessageType varchar(100) NOT NULL,
+        MessageInfo varchar(1000) NOT NULL
 );
