622ce74a21af21fafd919e35f7e6cb31e9178e80
Thomas Eberhart <thomas.eberhart@oit.edu>
2021-03-03 16:37:01 -0800

    Began work on UI for AddChallengePage

---
+++ NewRecord!/NewRecord_Backend/OfficialViews/AddChallengePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using NewRecord_Backend.ViewModels;
+
+namespace NewRecord_Backend.OfficialViews
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AddChallengePage : ContentPage
+	{
+		AddChallengeViewModel vm;
+		public AddChallengePage ()
+		{
+			InitializeComponent();
+			vm = new AddChallengeViewModel(Navigation);
+
+			BindingContext = vm;
+		}
+	}
+}
